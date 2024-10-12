d76b2530f7b46c9a03e229f491d95fb396864a35
Bushra Afaq <165610940+BUSHRS@users.noreply.github.com>
2024-10-12 11:51:21 +0500

    some changes

---
+++ countries/app/[countries-page]/pakistan/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+import pakistan from "../pakistan.jpg";
+
+export default function Pakistan(){
+    const data={
+        capital:"Islamabad",
+        language:"Urdu",
+        population:"456,09,765",
+
+    }
+    return(
+        <div
+    )
+}
---
+++ countries/app/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+export default function Home(){
+  return(
+    <div>
+      <center>
+        <ul>
+          <Link href="/">Home</Link>&nbsp; &nbsp;
+          <Link href="/">About</Link>&nbsp; &nbsp;
+          <Link href="/">country-page</Link>&nbsp; &nbsp;
+          <Link href="/">feedback</Link>&nbsp; &nbsp;
+          <br />
+          <br />
+
+         <h1>WELCOME TO GIAIC</h1>
+         <h2>SATURDAY SLOT 9.00 TO 12.00</h2>
+         <h2>MY TEACHERS ARE "BILAL MOHD AND ANEEQUE"</h2>
+        </ul>
+        
+
+
+      </center>
+    </div>
+  )
+}
